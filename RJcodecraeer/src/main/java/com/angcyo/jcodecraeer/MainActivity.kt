54ea0830007abfaaa2ccd2f1723f31e75a2ff919
angcyo <angcyo@126.com>
2018-02-08 15:20:58 +0800

    init

---
+++ RJcodecraeer/src/main/java/com/angcyo/jcodecraeer/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.angcyo.jcodecraeer
+
+import android.content.Intent
+import com.angcyo.uiview.base.UILayoutActivity
+
+class MainActivity : UILayoutActivity() {
+    override fun onLoadView(intent: Intent?) {
+
+    }
+}
